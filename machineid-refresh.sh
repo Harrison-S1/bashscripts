1b55b611d2d0e56fe9879088f44c065e88232451
Harrison-S1 <54186142+Harrison-S1@users.noreply.github.com>
2021-08-19 13:38:00 +0100

    Update machineid-refresh.sh

--- machineid-refresh.sh
+++ machineid-refresh.sh
@@ -6,5 +6,6 @@ dbus-uuidgen --ensure=/etc/machine-id
 dbus-uuidgen --ensure
 ######## not that if you put a @reboot in the cron you will need this last part, if not just remove it
 ##### cron would be @reboot /root/machineid-refresh.sh
-rm /root/chg.sh
 sed -i '$ d' /var/spool/cron/crontabs/root/
+rm /root/chg.sh
+
